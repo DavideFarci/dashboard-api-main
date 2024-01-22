f9da75a346801f0bda1cf46d1511de1bc304480e
DavideFarci <yank92.df@gmail.com>
2024-01-22 18:02:48 +0100

    fix order_controller

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Date;
 use App\Models\Order;
 use App\Models\Project;
+use App\Models\Category;
 use App\Models\OrderProject;
 use App\Models\projectOrder;
 use Illuminate\Http\Request;
@@ -24,26 +26,18 @@ class OrderController extends Controller
 
     public function store(Request $request)
     {
-
-        
-
         // salvare i dati del Order nel database
         $total_price = 0;
-        $total_pz = 0;
         $data = $request->all();
-        // $arrId= json_decode($data['arrId']);
-        // $arrQt= json_decode($data['arrQt']);
-        //$arrVar= json_decode($data['arrVariation'], true);
-        
+
         $arrvar = str_replace('\\', '', $data['arrVariation']);
-        $arrvar2 = json_decode($arrvar,true);
-    
-        
-        for($i = 0; $i < count($arrvar2); ++$i){
+        $arrvar2 = json_decode($arrvar, true);
+
+
+        for ($i = 0; $i < count($arrvar2); ++$i) {
             $project = Project::where('id', $arrvar2[$i]['p_id'])->first();
 
             $total_price += $project->price *  $arrvar2[$i]['counter'];
-            
         }
 
         $newOrder = new Order();
@@ -54,18 +48,39 @@ class OrderController extends Controller
         $newOrder->total_price   = $total_price;
         $newOrder->status        = 0;
         $newOrder->save();
-        
+
+        $pezzi = 0;
         foreach ($arrvar2 as $elem) {
             $item_order = new OrderProject();
             $item_order->order_id = $newOrder->id;
             $item_order->project_id = $elem['p_id'];
             $item_order->quantity_item = $elem['counter'];
-            $item_order->deselected= json_encode($elem['deselected']);
-            $item_order->addicted= json_encode($elem['addicted']);
+            $product = Project::where('id', $elem['p_id'])->first();
+            $category = Category::where('id', $product->category_id)->first();
+            if ($category->slot) {
+                $pezzi = ($pezzi + $elem['counter']) * $category->slot;
+            }
+            $item_order->deselected = json_encode($elem['deselected']);
+            $item_order->addicted = json_encode($elem['addicted']);
             $item_order->save();
-
         }
-     // ritornare un valore di successo al frontend
+
+        $date = Date::where('id', $data['date_id'])->firstOrFail();
+        $maximum = $date->reserved_pz + $pezzi;
+
+        if ($maximum <= $date->max_pz) {
+            $date->reserved_pz = $date->reserved_pz + $pezzi;
+            if ($date->reserved_pz == $date->max_pz) {
+                $date->visible = 0;
+            }
+        } else {
+            // se non ci sono più posti rispondo picche
+            return response()->json([
+                'success' => false,
+                'message' => 'Il numero massimo di pezzi per questa data e orario è già stato raggiunto',
+            ]);
+        }
+        // ritornare un valore di successo al frontend
         return response()->json([
             'success' => true,
         ]);
