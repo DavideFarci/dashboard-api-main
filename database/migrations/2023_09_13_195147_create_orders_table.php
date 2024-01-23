21681756388890da439fca03ae6bbc0d168f14db
DavideFarci <yank92.df@gmail.com>
2024-01-23 18:43:26 +0100

    fix migration

--- database/migrations/2023_09_13_195147_create_orders_table.php
+++ database/migrations/2023_09_13_195147_create_orders_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50)();
+            $table->string('name', 50);
             $table->string('phone', 20);
             $table->string('email', 100);
             $table->string('message', 1000)->nullable();
